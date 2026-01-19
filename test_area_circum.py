9e290f49f86d91165011b6b19fc35aa77f62312d
Jaron Devaraj <jaronelwin@gmail.com>
2026-01-19 10:08:02 +0530

    Update test_area_circum.py

--- test_area_circum.py
+++ test_area_circum.py
@@ -1,7 +1,7 @@
 import math
 from area_circum import area_circum
 
-def test_case():
+def test_area_circum():
     area, circumference = area_circum(5)
     assert area == math.pi * 25
     assert circumference == 2 * math.pi * 5
