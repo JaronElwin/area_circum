5bbafd5b9b7ad65faa66f4b63e3ddf663cb40791
Jaron Devaraj <jaronelwin@gmail.com>
2026-01-19 09:55:22 +0530

    Update area_circum.py

--- area_circum.py
+++ area_circum.py
@@ -1,7 +1,10 @@
 import math
 def area_circum(radius):
-    area = math.pi*radius*radius
-    circumference = 2*math.pi*radius
+    area = math.pi * radius * radius
+    circumference = 2 * math.pi * radius
     return area, circumference
+
 if __name__ == "__main__":
-    area_circum(radius=5)
+    area, circumference = area_circum(radius=5)
+    print(f"Area: {area:.2f}")
+    print(f"Circumference: {circumference:.2f}")
