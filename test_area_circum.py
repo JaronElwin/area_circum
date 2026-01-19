8d0320662012bebef9d9b9488f3b85d2aec195d6
Jaron Devaraj <jaronelwin@gmail.com>
2026-01-19 09:37:24 +0530

    Update test_area_circum.py

--- test_area_circum.py
+++ test_area_circum.py
@@ -1,5 +1,4 @@
 from area_circum import area_circum
 def test_case():
     area, circumference = area_circum(5)
-    assert area == 78.5
-    assert circumference == 31.4
+    assert area == 78.5, circumference == 31.4
