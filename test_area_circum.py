50cc834803317e333adc4e04f56c17f4b99bfad8
Jaron Devaraj <jaronelwin@gmail.com>
2026-01-19 09:57:56 +0530

    Add assertions for area and circumference calculations

--- test_area_circum.py
+++ test_area_circum.py
@@ -1,5 +1,7 @@
+import math
 from area_circum import area_circum
+
 def test_case():
     area, circumference = area_circum(5)
-    assert area
-    assert circumference 
+    assert math.isclose(area, math.pi * 25)
+    assert math.isclose(circumference, 2 * math.pi * 5)
